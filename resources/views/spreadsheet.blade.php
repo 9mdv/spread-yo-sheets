c766ed749619f059bfd5537a715ddb139c8cf99b
G <g11v8a@gmail.com>
2019-03-18 21:43:43 +0800

    added who's viewing feature 👀

--- resources/views/spreadsheet.blade.php
+++ resources/views/spreadsheet.blade.php
@@ -8,10 +8,29 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        .avatar {
+            color: rgb(255, 255, 255);
+            background-color: #fc0093;
+            display: inline-block;
+            font-family: Arial, sans-serif;
+            font-size: 20px;
+            border-radius: 50%;
+            width: 36px;
+            height: 36px;
+            text-align: center;
+        }
+    </style>
 </head>
 
 <br>
 <h2>{{ $sheet->name }}</h2>
+<p>
+    <span style="float: right; margin-right: 50px; margin-bottom: 40px; font-size: 16px;">Now viewing: <span id="viewers"></span>
+    </span>
+</p>
+<br> <br>
+
 <div id="sheet"></div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/handsontable/2.0.0/handsontable.min.js"></script>
@@ -44,6 +63,7 @@
     });
 
 </script>
+<script src="{{ asset('js/app.js') }}"></script>
 <script src="https://js.pusher.com/4.2/pusher.min.js"></script>
 <script>
     let pusher = new Pusher("{{ env('PUSHER_APP_KEY') }}", {
@@ -57,11 +77,17 @@
     });
 
     pusher.subscribe("{{ $sheet->channel_name }}")
+        .bind('pusher:subscription_succeeded', (data) => {
+            Object.entries(data.members)
+                .forEach(([id, member]) => addViewer(member));
+        })
+        .bind('pusher:member_added', (member) => addViewer(member.info))
+        .bind('pusher:member_removed', (member) => removeViewer(member))
         .bind('updated', function (message) {
             let [rowIndex, columnIndex, oldValue, newValue] = message.change;
             addCellValue(rowIndex, columnIndex, newValue);
             table.loadData(sheetContent);
-    });
+        });
 
     function addCellValue(rowIndex, columnIndex, newValue) {
         // we expand the sheet to reach the farthest cell
@@ -74,6 +100,29 @@
         }
 
         sheetContent[rowIndex][columnIndex] = newValue;
+    }
+
+</script>
+<script>
+    function addViewer(viewer) {
+        const userInitials = viewer.name.split(' ')
+            .reduce((initials, name) => {
+                initials.push(name[0]);
+                return initials;
+            }, []).join('');
+        let $avatar = $('<span>')
+            .addClass('avatar')
+            .attr('data-toggle', 'tooltip')
+            .attr('id', `avatar-${viewer._id}`)
+            .attr('title', viewer.name)
+            .text(userInitials);
+        $('#viewers').append($avatar);
+        // enable the tooltip
+        $avatar.tooltip();
+    }
+
+    function removeViewer(viewer) {
+        $(`#avatar-${viewer.id}`).remove();
     }
 
 </script>
